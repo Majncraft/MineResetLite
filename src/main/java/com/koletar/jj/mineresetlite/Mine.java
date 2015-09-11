e04e69436e97048054e38382c20b47c09187d096
Welfare93 <frycjiri@gmail.com>
2015-09-12 01:19:09 +0200

    Update Mine.java

--- src/main/java/com/koletar/jj/mineresetlite/Mine.java
+++ src/main/java/com/koletar/jj/mineresetlite/Mine.java
@@ -337,7 +337,10 @@ public class Mine implements ConfigurationSerializable {
 		for (int x = minX; x <= maxX; ++x) {
 			for (int y = minY; y <= maxY; ++y) {
 				for (int z = minZ; z <= maxZ; ++z) {
-					if (!fillMode || world.getBlockAt(x, y, z).getType()==Material.AIR || world.getBlockAt(x, y, z).getType()==Material.WATER|| (world.getBlockAt(x, y,z ).getType()!=Material.STATIONARY_WATER && world.getBlockAt(x, y, z).getState().getRawData()>0)) {
+					if (!fillMode || 
+          world.getBlockAt(x, y, z).getType()==Material.AIR 
+          || world.getBlockAt(x, y, z).getType()==Material.WATER
+          || (world.getBlockAt(x, y,z ).getType()==Material.STATIONARY_WATER && world.getBlockAt(x, y, z).getState().getRawData()>0)) {
 						if(world.getBlockTypeIdAt(x, y, z) == 65 & ignoreLadders) {
 							continue;
 						}
